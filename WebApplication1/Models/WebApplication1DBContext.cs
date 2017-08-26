af303225fbc8ec159c53d4413534c9a30baccbb8
Brad <bradcopenhaver@gmail.com>
2017-08-25 17:08:24 -0700

    add dbcontext class and fiddle with adding EF nugets and connection strings

---
+++ WebApplication1/Models/WebApplication1DBContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models
+{
+    public class WebApplication1DBContext : DbContext
+    {
+        public virtual DbSet<Thing> Things { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder
+                .UseMySql(@"Server=localhost;database=WebApplication1DB;uid=root;pwd=S3ph!roth;");
+    }
+}
